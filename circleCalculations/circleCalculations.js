f7a2c8f3c8f149cac858cb2c091d23fafe1fdf83
Laura Hackney <laurahackney@Lauras-MacBook-Pro.local>
2016-03-22 09:35:20 -0700

    circleCalculations answers with Bonus

--- circleCalculations/circleCalculations.js
+++ circleCalculations/circleCalculations.js
@@ -1,5 +1,12 @@
 //Write a JavaScript program to calculate the area and perimeter of a circle. The radius of the circle will be supplied by the user.
 
-function circleCalculation () {
-	
+//Bonus: Write the answer like this "The area of the circle is ___ and the perimeter of the circle is ____"
+//Bonus: Make both numbers whole integers (aka round to the nearest integer) 
+
+function circleCalculation (radius) {
+	var area = Math.round(Math.PI * 2 * radius);
+	var perimeter = Math.round(Math.PI * radius * radius);
+	console.log("The area of the circle is " + area +", and the perimeter is " + perimeter +".");
 }
+
+circleCalculation(10);
