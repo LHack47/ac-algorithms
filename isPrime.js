ca516fddd6d29f5e8c9d9d398480827cbcfb5dd2
Laura Hackney <laurahackney@Lauras-MacBook-Pro.local>
2016-04-02 11:20:40 -0700

    my answers to isPrime.js

--- isPrime.js
+++ isPrime.js
@@ -32,5 +32,13 @@ console.log(result) // false
 **/
 
 function isPrime(num) {
-	// Your code
+  if (num < 2){
+    return false;
+  }  
+  for(var i = 2; i < num; i++) {
+        if(num % i === 0) {
+            return false;
+        }
+    }
+    return true;
 }
